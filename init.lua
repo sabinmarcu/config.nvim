15422183b334c5f9c52d5c27b24cfb2e8f079348
Sabin Marcu <sabin.marcu@hootsuite.com>
2023-01-30 12:14:32 +0200

    feat: name the test category

--- init.lua
+++ init.lua
@@ -148,6 +148,7 @@ local config = {
           -- third key is the key to bring up next level and its displayed
           -- group name in which-key top level menu
           ["b"] = { name = "Buffer" },
+          ["j"] = { name = "Test" }
         },
       },
     },
