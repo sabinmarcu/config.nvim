545dc9e5dae77e6f4ea8051a62968fb079f5a22a
Sabin Marcu <sabinmarcu@gmail.com>
2023-02-04 04:46:27 +0200

    fix: update custom nvim config path in zsh script

--- custom/zsh.lua
+++ custom/zsh.lua
@@ -2,6 +2,6 @@ local config_file_path = vim.fn.expand('$HOME/.pre.zshrc.nvim.local')
 local config_file = io.open(config_file_path, 'w')
 if (config_file) then
   io.output(config_file)
-  io.write('export NVIM_CUSTOM="$HOME/.config/nvim/lua/custom"')
+  io.write('export NVIM_CUSTOM="$HOME/.config/nvim/lua/user"')
   io.close(config_file)
 end
