63522b122b9c893c956b0087af52b27c99487e6c
Sabin Marcu <sabinmarcu@gmail.com>
2023-05-25 08:30:32 +0300

    feat: fix comment plugin and add codedoc plugin

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -5,32 +5,29 @@ return {
       { "<leader>/", false },
     },
   },
-  -- {
-  --   "echasnovski/mini.comment",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     mappings = {
-  --       comment = "gc",
-  --       comment_line = "<Leader>/",
-  --       textobject = "gc",
-  --     },
-  --   },
-  -- },
   {
-    "numToStr/Comment.nvim",
-    keys = {
-      -- { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
-      -- { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
-      {
-        "<leader>/",
-        mode = { "n", "v" },
-        "<cmd>lua require('Comment.api').toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)<CR>",
-        desc = "Comment selection or line",
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        comment = "<leader>/",
+        comment_line = "<leader>/",
+        textobject = "gt",
       },
     },
-    -- opts = function()
-    --   local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
-    --   return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
-    -- end,
+  },
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          require("neogen").generate()
+        end,
+        desc = "Generate Documentation",
+      },
+    },
   },
 }
