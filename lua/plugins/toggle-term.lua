a399f52ee511dc3856c94c6be4fe666067797cc3
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-25 18:38:39 +0300

    feat: add terminal opening from neo-tree

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,5 +1,53 @@
 return {
   {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      commands = {
+        open_terminal = function(state)
+          local tree = state.tree
+          local success, node = pcall(tree.get_node, tree)
+          if node.type == "message" then
+            return
+          end
+          if not (success and node) then
+            return
+          end
+          local pathToOpen = node.path
+          if node.type ~= "directory" then
+            pathToOpen = string.match(pathToOpen, "^(.*)/")
+          end
+          local Terminal = require("toggleterm.terminal").Terminal
+          local newTerm = Terminal:new({
+            dir = pathToOpen,
+            direction = "float",
+            on_open = function(term)
+              vim.cmd("startinsert!")
+              vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+            end,
+            on_close = function()
+              vim.cmd("startinsert!")
+            end,
+          })
+          newTerm:open()
+        end,
+      },
+      window = {
+        mappings = {
+          ["t"] = "open_terminal",
+        },
+      },
+    },
+  },
+  {
     "akinsho/toggleterm.nvim",
     version = "*",
     config = true,
