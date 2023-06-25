e42da50f753123d32acb93673837cf93ed6e6260
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-25 18:37:21 +0300

    fix: move shade keymap

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
         keys = {
           brightness_up = "<C-Up>",
           brightness_down = "<C-Down>",
-          toggle = "<Leader>s",
+          toggle = "<Leader>uS",
         },
       })
     end,
