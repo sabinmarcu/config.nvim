eb0e87e005811a30e0fa1a84732089a5128d2587
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-29 03:10:22 +0300

    feat: update neotest keys

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -6,7 +6,6 @@ return {
     "antoinemadec/FixCursorHold.nvim",
     "haydenmeade/neotest-jest",
   },
-  event = "User AstroFile",
   config = function()
     require("neotest").setup({
       adapters = {
@@ -21,4 +20,76 @@ return {
       },
     })
   end,
+  keys = {
+    {
+      "<leader>ja",
+      function()
+        require("neotest").run.attach()
+      end,
+      desc = "Attach",
+    },
+    {
+      "<leader>jf",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Run file",
+    },
+    {
+      "<leader>jF",
+      function()
+        require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" })
+      end,
+      desc = "Debug file",
+    },
+    {
+      "<leader>jl",
+      function()
+        require("neotest").run.run_last()
+      end,
+      desc = "Run last",
+    },
+    {
+      "<leader>jL",
+      function()
+        require("neotest").run.run_last({ strategy = "dap" })
+      end,
+      desc = "Debug last",
+    },
+    {
+      "<leader>jn",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run nearest",
+    },
+    {
+      "<leader>jN",
+      function()
+        require("neotest").run.run({ strategy = "dap" })
+      end,
+      desc = "Debug nearest",
+    },
+    {
+      "<leader>jo",
+      function()
+        require("neotest").output.open({ enter = true })
+      end,
+      desc = "Output Window",
+    },
+    {
+      "<leader>js",
+      function()
+        require("neotest").run.stop()
+      end,
+      desc = "Stop",
+    },
+    {
+      "<leader>jS",
+      function()
+        require("neotest").summary.toggle()
+      end,
+      desc = "Summary",
+    },
+  },
 }
