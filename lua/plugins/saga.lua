f23b06818cea7dd1651d2e6dcce1458d4e2ab274
Sabin Marcu <sabin.marcu@platform24.com>
2024-02-06 11:06:55 +0200

    feat: add lspsaga

---
+++ lua/plugins/saga.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvimdev/lspsaga.nvim",
+    config = {
+      symbols_in_winbar = {
+        enable = true,
+      },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
+}
