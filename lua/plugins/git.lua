3b455571ce82bb9bd046aeaeefab0a4c8f7a92eb
Sabin Marcu <sabin.marcu@platform24.com>
2024-03-22 19:05:03 +0100

    feat: add git file history telescope extension

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,4 +2,25 @@ return {
   {
     "f-person/git-blame.nvim",
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "isak102/telescope-git-file-history.nvim",
+        dependencies = { "tpope/vim-fugitive" },
+        config = function()
+          require("telescope").load_extension("git_file_history")
+        end,
+        keys = {
+          {
+            "<leader>gf",
+            function()
+              require("telescope").extensions.git_file_history.git_file_history()
+            end,
+            desc = "Git file history",
+          },
+        },
+      },
+    },
+  },
 }
