ec796f77fd39e4c4e526befce1a97135b8291e80
Sabin Marcu <sabin.marcu@platform24.com>
2024-04-17 16:47:38 +0300

    feat: use nvim-surround instead of mini.surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,7 @@
 return {
   {
     "echasnovski/mini.surround",
+    enabled = false,
     opts = {
       mappings = {
         add = "S",
@@ -9,6 +10,29 @@ return {
         find_left = "fsl",
         highlight = "hs",
         replace = "cs",
+      },
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    -- enabled = false,
+    version = "*",
+    event = "VeryLazy",
+    config = true,
+    vscode = true,
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
       },
     },
   },
