73272345ba3871d8e3039f8b3f25a25744e6ff1c
Sabin Marcu <sabin.marcu@platform24.com>
2024-04-17 16:51:15 +0300

    feat: update oil.nvim plugin

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -2,11 +2,24 @@ return {
   {
     "stevearc/oil.nvim",
     config = true,
+    opts = {
+      keymaps = {
+        ["s"] = "actions.select_vsplit",
+        ["S"] = "actions.select_split",
+      },
+    },
     keys = {
       {
         "<leader>fo",
         function()
           require("oil").toggle_float()
+        end,
+        desc = "Open in Oil",
+      },
+      {
+        "<leader>fO",
+        function()
+          require("oil").open()
         end,
         desc = "Open in Oil",
       },
