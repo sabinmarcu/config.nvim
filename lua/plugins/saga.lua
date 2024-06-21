cdb50e07d70dabcfa4e0d9413655ca618ce7b6ce
Sabin Marcu <sabin.marcu@platform24.com>
2024-06-21 15:11:08 +0300

    feat: disable lspsaga

--- lua/plugins/saga.lua
+++ lua/plugins/saga.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    "nvimdev/lspsaga.nvim",
-    config = {
-      symbols_in_winbar = {
-        enable = true,
-      },
-    },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
+  -- {
+  --   "nvimdev/lspsaga.nvim",
+  --   config = {
+  --     symbols_in_winbar = {
+  --       enable = true,
+  --     },
+  --   },
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  -- },
 }
