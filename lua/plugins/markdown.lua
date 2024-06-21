8e7e52119e5e74425ec4a37e9908f4eddd552485
Sabin Marcu <sabin.marcu@platform24.com>
2024-06-21 15:11:51 +0300

    feat: add mdx support

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -8,4 +8,41 @@ return {
     end,
     ft = { "markdown" },
   },
+  {
+    "denstiny/styledoc.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "vhyrro/luarocks.nvim",
+      "3rd/image.nvim",
+    },
+    opts = true,
+    ft = "markdown",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "markdown",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      vim.filetype.add({
+        extension = {
+          mdx = "markdown.mdx",
+          ["markdown.mdx"] = "mdx",
+        },
+      })
+      vim.treesitter.language.register("javascript", "markdown.mdx")
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ["mdx_analyzer"] = {},
+      },
+    },
+  },
 }
