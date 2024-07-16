10d5105a6b51d821294d468d28da9afa24da7be4
Sabin Marcu <sabin.marcu@platform24.com>
2024-07-16 11:23:23 +0300

    feat: update neovide mappings

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -6,3 +6,10 @@ vim.g.neovide_remember_window_size = true
 
 vim.g.neovide_floating_blur_amount_x = 6.0
 vim.g.neovide_floating_blur_amount_y = 6.0
+
+local map = vim.keymap
+
+map.set({ "n", "v" }, "<D-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+map.set({ "n", "v" }, "<D-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+map.set({ "n", "v" }, "<D-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+map.set({ "n", "v" }, "<D-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
