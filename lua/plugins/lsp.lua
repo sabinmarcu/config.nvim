bbc1f6bfe3b72d31b23429e2a4656cda6e6ddca7
Sabin Marcu <sabin.marcu@platform24.com>
2024-08-13 14:28:25 +0300

    feat: disable inline hints by default

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      inlay_hints = { enabled = true },
+      inlay_hints = { enabled = false },
       servers = {
         eslint = {
           experimental = {
