a867279c848d5535e6e1ed63f6991f3101e06611
Sabin Marcu <sabin.marcu@platform24.com>
2024-09-10 11:47:55 +0300

    feat: add mini files

---
+++ lua/plugins/mini.files.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>fm", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+      { "<leader>fM", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+      { "<leader>e", false },
+      { "<leader>E", false },
+    },
+  },
+  {
+    "echasnovski/mini.files",
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+        "<leader>E",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
+      },
+    },
+  },
+}
