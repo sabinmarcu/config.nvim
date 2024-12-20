641555f4e759d14d593a428db9d9d064810da7d3
Sabin Marcu <sabin.marcu@platform24.com>
2024-12-20 16:56:36 +0200

    feat: add more devicons for mini.icons

--- lua/plugins/devicons.lua
+++ lua/plugins/devicons.lua
@@ -7,4 +7,18 @@ return {
     event = "VeryLazy",
     config = true,
   },
+  {
+    "echasnovski/mini.icons",
+    opts = {
+      file = {
+        ["eslint.config.mjs"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        ["eslint.config.cjs"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+        ["tsconfig.eslint.json"] = { glyph = "", hl = "MiniIconsAzure" },
+        ["jest.config.mjs"] = { glyph = "", hl = "MiniIconsOrange" },
+        ["jest.config.options.mjs"] = { glyph = "", hl = "MiniIconsOrange" },
+        ["jest.config.project.mjs"] = { glyph = "", hl = "MiniIconsOrange" },
+        [".gitlab.workspaces.yml"] = { glyph = "󰮠 ", hl = "MiniIconsOrange" },
+      },
+    },
+  },
 }
