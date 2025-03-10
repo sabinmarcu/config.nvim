116638dd292ebffe26a1a28b4b25e6ce9560e36d
Sabin Marcu <sabin.marcu@platform24.com>
2025-03-10 10:31:09 +0200

    feat: remove neogen from own config

--- lua/plugins/comments.lua
+++ lua/plugins/comments.lua
@@ -20,18 +20,4 @@ return {
     event = "VeryLazy",
     enabled = vim.fn.has("nvim-0.10.0") == 1,
   },
-  {
-    "danymat/neogen",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true,
-    keys = {
-      {
-        "<leader>cD",
-        function()
-          require("neogen").generate()
-        end,
-        desc = "Generate Documentation",
-      },
-    },
-  },
 }
