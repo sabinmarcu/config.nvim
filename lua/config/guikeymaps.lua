44e5efb7fc7a8b99b84f7f5784fd2922be62b738
Sabin Marcu <sabin.marcu@platform24.com>
2025-03-10 10:30:53 +0200

    feat: fix guikeymaps

--- lua/config/guikeymaps.lua
+++ lua/config/guikeymaps.lua
@@ -1,5 +1,6 @@
 local mapping = require("mapping")
 
+-- Copy + Paste
 mapping.map("n", "<D-c>", '"*y :let @+=@*<CR>', { silent = true })
 mapping.map("n", "<D-v>", '"+p', { silent = true })
 mapping.map("i", "<D-c>", '"*y :let @+=@*<CR>', { silent = true })
@@ -7,10 +8,16 @@ mapping.map("i", "<D-v>", '<esc>"+pa', { silent = true })
 mapping.map("v", "<D-c>", '"*y :let @+=@*<CR>', { silent = true })
 mapping.map("v", "<D-v>", '"+p', { silent = true })
 
-mapping.map("n", "<D-z>", "<cmd>undo<cr>", { silent = true })
-mapping.map("i", "<D-z>", "<esc><cmd>undo<cr>", { silent = true })
-mapping.map("n", "<D-S-z>", "u", { silent = true })
-mapping.map("i", "<D-S-z>", "u", { silent = true })
+-- Redo
+mapping.map("n", "<ds-z>", "<cmd>redo<cr>", { silent = true })
+mapping.map("i", "<ds-z>", "<esc><cmd>redo<cr>", { silent = true })
 
+-- Undo
+mapping.map("n", "<d-z>", "<cmd>undo<cr>", { silent = true })
+mapping.map("i", "<d-z>", "<esc><cmd>undo<cr>", { silent = true })
+
+-- Save
 mapping.map("n", "<D-s>", "<cmd>w<cr>", { silent = true })
+
+-- Close Buffer
 mapping.map("n", "<D-w>", "<cmd>bd<cr>", { silent = true })
