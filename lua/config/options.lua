5b6de547a57aa9b093a9f8cf6748b7745d05ea53
Sabin Marcu <sabin.marcu@platform24.com>
2025-03-13 02:59:23 +0200

    feat: disable ghost_line for blink.cmp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,7 @@ vim.opt.wrap = true
 vim.g.root_spec = { { ".git", "lua" }, "lsp", "cwd" }
 vim.g.lazyvim_prettier_needs_config = true
 vim.o.autoread = true
+vim.g.ai_cmp = false
 
 if vim.g.neovide then
   require("config.neovide")
