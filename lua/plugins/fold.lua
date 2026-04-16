35cc115edb0171fdc6d5450b3af703d70cf096e4
Sabin Marcu <sabinmarcu@gmail.com>
2026-04-16 04:21:09 +0300

    fix: temporarily disable ufo

--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -1,48 +1,48 @@
 return {
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-      "kevinhwang91/promise-async",
-    },
-    init = function()
-      vim.o.foldenable = true
-      vim.o.foldcolumn = "0" -- '0' is not bad
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.fillchars = "eob: ,fold: ,foldopen:⌄,foldsep:|,foldclose:›"
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      }
-      local language_servers = require("lspconfig").util.available_servers()
-      for _, ls in ipairs(language_servers) do
-        require("lspconfig")[ls].setup({
-          capabilities = capabilities,
-        })
-      end
-    end,
-    opts = {
-      provider_selector = function()
-        return { "treesitter", "indent" }
-      end,
-    },
-    keys = {
-      {
-        "zR",
-        function()
-          require("ufo").openAllFolds()
-        end,
-        desc = "Open all folds",
-      },
-      {
-        "zM",
-        function()
-          require("ufo").closeAllFolds()
-        end,
-        desc = "Close all folds",
-      },
-    },
-  },
+  -- {
+  --   "kevinhwang91/nvim-ufo",
+  --   dependencies = {
+  --     "kevinhwang91/promise-async",
+  --   },
+  --   init = function()
+  --     vim.o.foldenable = true
+  --     vim.o.foldcolumn = "0" -- '0' is not bad
+  --     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+  --     vim.o.foldlevelstart = 99
+  --     vim.o.fillchars = "eob: ,fold: ,foldopen:⌄,foldsep:|,foldclose:›"
+  --
+  --     local capabilities = vim.lsp.protocol.make_client_capabilities()
+  --     capabilities.textDocument.foldingRange = {
+  --       dynamicRegistration = false,
+  --       lineFoldingOnly = true,
+  --     }
+  --     local language_servers = require("lspconfig").util.available_servers()
+  --     for _, ls in ipairs(language_servers) do
+  --       require("lspconfig")[ls].setup({
+  --         capabilities = capabilities,
+  --       })
+  --     end
+  --   end,
+  --   opts = {
+  --     provider_selector = function()
+  --       return { "treesitter", "indent" }
+  --     end,
+  --   },
+  --   keys = {
+  --     {
+  --       "zR",
+  --       function()
+  --         require("ufo").openAllFolds()
+  --       end,
+  --       desc = "Open all folds",
+  --     },
+  --     {
+  --       "zM",
+  --       function()
+  --         require("ufo").closeAllFolds()
+  --       end,
+  --       desc = "Close all folds",
+  --     },
+  --   },
+  -- },
 }
